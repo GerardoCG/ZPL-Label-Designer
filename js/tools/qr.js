aad6f5aca3495ca620e14ecd9e6b5505119a69c2
Gerardo Contreras Gutierrez <gerardo305@gmail.com>
2019-08-11 23:22:04 -0700

    QR option errors were resolved.

--- js/tools/qr.js
+++ js/tools/qr.js
@@ -10,37 +10,39 @@ if (!com.logicpartners.designerTools)
         this.counter = 1;
         this.button = $("<div></div>").addClass("designerToolbarQR designerToolbarButton").attr("title", "QR").append($("<div></div>"));
         this.object = function(x, y, width, height) {
-            var width = 100;
-            var canvasHolder = $("<canvas></canvas>").prop("width", "100").prop("height", "1");
+            var width = 90;
+            var canvasHolder = $("<canvas></canvas>").prop("width", "90", "height", "90").prop("height", "1");
             this.name = "QR " + self.counter++;
             this.text = "QR";
             this.x = x;
             this.y = y;
-            //this.width = width;
             this.height = height;
-            
+            this.qr = "";
+            this.qrImage = new Image();
+            this.id = "qr-img-"+self.counter;
+            this.imgQR;
+
+            $("#imgQR").append("<div id='"+this.id+"'></diV>");
+            this.imgQR = new QRCode(this.id, {
+                text : this.text,
+                width : 100,
+                height : 100
+            });
+
             this.getZPLData = function() {
                 return "";
             }
     
             this.toZPL = function(labelx, labely, labelwidth, labelheight) {
-                return "XA^FO100,100^BQN,2,10^FDMM,AAC-45^FS^XS";
+                return "ESTO ES CODIGO PARA GENERAR QR";//"XA^FO100,100^BQN,2,10^FDMM,AAC-45^FS^XS";
             }
     
-            this.draw = function(context) {
-                var ctx = canvasHolder[0].getContext('2d');
-                //canvasHolder.QRCode("labelDesigner", { text : this.text, width: 1, height : 1});
-                $("#imgQR").html(" ");
-                var imgQR = new QRCode("imgQR", {
-                        text : "http://naver.com",
-                        width : 90,
-                        height : 90
-                });
-                var image = new Image();
-                image.onload = function() {
-                    ctx.drawImage(image, 0, 0);
-                  };
-                image.src = imgQR._el.lastChild.src;
+            this.draw = function(context) {             
+                this.imgQR.clear();
+                this.imgQR.makeCode(this.text);
+                this.qr = $("#"+this.id).find("img").attr("src");
+                this.qrImage.src = this.qr;
+                context.drawImage(this.qrImage, this.x, this.y, this.height, this.height);
             }
             
             this.setWidth = function(width) {
